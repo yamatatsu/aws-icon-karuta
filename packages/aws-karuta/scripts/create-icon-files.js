c09e4f88de1c5b6c4efacd2a848f72a7c2a2f0bc
yamatatsu <the.kinnikuman@gmail.com>
2022-12-14 01:05:09 +0900

    feat: icon index file generator

---
+++ packages/aws-karuta/scripts/create-icon-files.js
@@ -0,0 +1,61 @@
+import path from "node:path";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import camelCase from "camelcase";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const iconsDir = path.resolve(__dirname, "../src/icons");
+
+createIndexFiles(findSVG(iconsDir));
+
+function createIndexFiles(tree) {
+  return Object.entries(tree).forEach(([_path, subTree]) => {
+    if (Array.isArray(subTree)) {
+      createFile(
+        _path,
+        subTree
+          .filter((fileName) => fileName !== ".DS_Store")
+          .map((fileName) => {
+            const moduleName = camelCase(fileName.replace(/\&/g, ""));
+            return `export { ReactComponent as ${moduleName} } from './${fileName}';`;
+          })
+      );
+    } else {
+      createFile(
+        _path,
+        Object.keys(subTree)
+          .map((_path) => _path.match(/\/([^/]+)$/)[1])
+          .map((dirName) => {
+            const moduleName = Number.isNaN(Number(dirName))
+              ? camelCase(dirName)
+              : `_${dirName}`;
+
+            return `export * as ${moduleName} from './${dirName}';`;
+          })
+      );
+      createIndexFiles(subTree);
+    }
+  });
+}
+
+function createFile(_path, lines) {
+  fs.writeFileSync(`${_path}/index.ts`, lines.join("\n"));
+}
+
+function findSVG(dirPath) {
+  const children = fs.readdirSync(dirPath);
+  const [, dirName] = dirPath.match(/\/([^/]+)$/);
+
+  if (children.every((child) => fs.lstatSync(`${dirPath}/${child}`).isFile())) {
+    return children.filter((fileName) => /\.svg$/.test(fileName));
+  } else {
+    return children.reduce((acc, child) => {
+      const childPath = `${dirPath}/${child}`;
+      if (fs.lstatSync(childPath).isFile()) {
+        return acc;
+      }
+      return { ...acc, [childPath]: findSVG(childPath) };
+    }, {});
+  }
+}
---
+++ packages/aws-karuta/src/icons/README.md
@@ -0,0 +1,1 @@
+This icons is downloaded from `Asset Package` link in [AWS Architecture Icons](https://aws.amazon.com/architecture/icons/).
