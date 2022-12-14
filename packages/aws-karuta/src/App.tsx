ec74d85ba538a90aba9d415fc155d26bd38614ad
yamatatsu <the.kinnikuman@gmail.com>
2022-12-14 09:12:46 +0900

    chore: categorize icons

--- packages/aws-karuta/src/App.tsx
+++ packages/aws-karuta/src/App.tsx
@@ -1,13 +1,53 @@
-import React from "react";
 import * as icons from "./icons";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      {flat(icons).map((icon, index) => (
-        <img key={index} src={icon} className="logo react" alt="React logo" />
-      ))}
+      <div>
+        <h2>Service 16 icons</h2>
+        {flat(icons.service16Icons).map((icon, index) => (
+          <img key={index} src={icon} className="logo react" alt="React logo" />
+        ))}
+      </div>
+      <div>
+        <h2>Service 32 icons</h2>
+        {flat(icons.service32Icons).map((icon, index) => (
+          <img key={index} src={icon} className="logo react" alt="React logo" />
+        ))}
+      </div>
+      <div>
+        <h2>Service 64 icons</h2>
+        {flat(icons.service64Icons).map((icon, index) => (
+          <img key={index} src={icon} className="logo react" alt="React logo" />
+        ))}
+      </div>
+
+      <div>
+        <h2>Category 16 icons</h2>
+        {flat(icons.category16Icons).map((icon, index) => (
+          <img key={index} src={icon} className="logo react" alt="React logo" />
+        ))}
+      </div>
+      <div>
+        <h2>Category 32 icons</h2>
+        {flat(icons.category32Icons).map((icon, index) => (
+          <img key={index} src={icon} className="logo react" alt="React logo" />
+        ))}
+      </div>
+      <div>
+        <h2>Category 64 icons</h2>
+        {flat(icons.category64Icons).map((icon, index) => (
+          <img key={index} src={icon} className="logo react" alt="React logo" />
+        ))}
+      </div>
+
+      <div>
+        <h2>Resource icons</h2>
+        {flat(icons.resourceIcons).map((icon, index) => (
+          <img key={index} src={icon} className="logo react" alt="React logo" />
+        ))}
+      </div>
     </div>
   );
 }
