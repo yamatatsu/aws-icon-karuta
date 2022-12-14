50a2357d627863577db8605d4bd8a547125ccd6f
yamatatsu <the.kinnikuman@gmail.com>
2022-12-14 10:00:45 +0900

    feat: paper css

--- packages/aws-karuta/src/App.tsx
+++ packages/aws-karuta/src/App.tsx
@@ -1,51 +1,92 @@
 import * as icons from "./icons";
-import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <div>
-        <h2>Service 16 icons</h2>
+    <div className="paper container">
+      <h2>Service 16 icons</h2>
+      <div className="row">
         {flat(icons.service16Icons).map((icon, index) => (
-          <img key={index} src={icon} className="logo react" alt="React logo" />
+          <img
+            key={index}
+            src={icon}
+            className="no-border"
+            style={{ height: "4em", margin: "0.5em" }}
+            alt="React logo"
+          />
         ))}
       </div>
-      <div>
-        <h2>Service 32 icons</h2>
+      <h2>Service 32 icons</h2>
+      <div className="row">
         {flat(icons.service32Icons).map((icon, index) => (
-          <img key={index} src={icon} className="logo react" alt="React logo" />
+          <img
+            key={index}
+            src={icon}
+            className="no-border"
+            style={{ height: "4em", margin: "0.5em" }}
+            alt="React logo"
+          />
         ))}
       </div>
-      <div>
-        <h2>Service 64 icons</h2>
+      <h2>Service 64 icons</h2>
+      <div className="row">
         {flat(icons.service64Icons).map((icon, index) => (
-          <img key={index} src={icon} className="logo react" alt="React logo" />
+          <img
+            key={index}
+            src={icon}
+            className="no-border"
+            style={{ height: "4em", margin: "0.5em" }}
+            alt="React logo"
+          />
         ))}
       </div>
 
-      <div>
-        <h2>Category 16 icons</h2>
+      <h2>Category 16 icons</h2>
+      <div className="row">
         {flat(icons.category16Icons).map((icon, index) => (
-          <img key={index} src={icon} className="logo react" alt="React logo" />
+          <img
+            key={index}
+            src={icon}
+            className="no-border"
+            style={{ height: "4em", margin: "0.5em" }}
+            alt="React logo"
+          />
         ))}
       </div>
-      <div>
-        <h2>Category 32 icons</h2>
+      <h2>Category 32 icons</h2>
+      <div className="row">
         {flat(icons.category32Icons).map((icon, index) => (
-          <img key={index} src={icon} className="logo react" alt="React logo" />
+          <img
+            key={index}
+            src={icon}
+            className="no-border"
+            style={{ height: "4em", margin: "0.5em" }}
+            alt="React logo"
+          />
         ))}
       </div>
-      <div>
-        <h2>Category 64 icons</h2>
+      <h2>Category 64 icons</h2>
+      <div className="row">
         {flat(icons.category64Icons).map((icon, index) => (
-          <img key={index} src={icon} className="logo react" alt="React logo" />
+          <img
+            key={index}
+            src={icon}
+            className="no-border"
+            style={{ height: "4em", margin: "0.5em" }}
+            alt="React logo"
+          />
         ))}
       </div>
 
-      <div>
-        <h2>Resource icons</h2>
+      <h2>Resource icons</h2>
+      <div className="row">
         {flat(icons.resourceIcons).map((icon, index) => (
-          <img key={index} src={icon} className="logo react" alt="React logo" />
+          <img
+            key={index}
+            src={icon}
+            className="no-border"
+            style={{ height: "4em", margin: "0.5em" }}
+            alt="React logo"
+          />
         ))}
       </div>
     </div>
