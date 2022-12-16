5e194d9a8262d4c181f9775945f7c99666899184
yamatatsu <the.kinnikuman@gmail.com>
2022-12-16 10:24:40 +0900

    feat: github link

--- packages/aws-karuta/src/Template.tsx
+++ packages/aws-karuta/src/Template.tsx
@@ -5,16 +5,57 @@ type Props = { children?: React.ReactNode };
 export default function Template(props: Props) {
   return (
     <>
-      <nav className="border fixed split-nav">
-        <div className="nav-brand">
-          <h3>
-            <Link to="/">Icon Karuta</Link>
-          </h3>
-        </div>
-      </nav>
+      <NavHeader />
       <div className="container" style={{ marginTop: "5em" }}>
         {props.children}
       </div>
+    </>
+  );
+}
+
+function NavHeader() {
+  return (
+    <nav className="border fixed split-nav">
+      <div className="nav-brand">
+        <h3>
+          <Link to="/">Icon Karuta</Link>
+        </h3>
+      </div>
+      <Menu
+        items={[
+          <a target="_blank" href="https://github.com/yamatatsu/aws-karuta">
+            Github
+          </a>,
+        ]}
+      />
+    </nav>
+  );
+}
+
+function Menu({ items }: { items: React.ReactNode[] }) {
+  return (
+    <div className="collapsible">
+      <Hamburger />
+      <div className="collapsible-body">
+        <ul className="inline">
+          {items.map((i, index) => (
+            <li key={index}>{i}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Hamburger() {
+  return (
+    <>
+      <input id="collapsible1" type="checkbox" name="collapsible1" />
+      <label htmlFor="collapsible1">
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
+      </label>
     </>
   );
 }
