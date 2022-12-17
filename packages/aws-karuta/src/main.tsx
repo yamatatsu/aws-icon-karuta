423ec2baccf450491f5b9cddc1277f98424babf7
yamatatsu <the.kinnikuman@gmail.com>
2022-12-17 22:37:53 +0900

    chore: use papercss as npm lib

--- packages/aws-karuta/src/main.tsx
+++ packages/aws-karuta/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
+import "papercss/dist/paper.min.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
