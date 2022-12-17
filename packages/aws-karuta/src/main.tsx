9ce0635097995572eda47914b7f2293321e25c5b
yamatatsu <the.kinnikuman@gmail.com>
2022-12-17 21:59:26 +0900

    chore: remove unused lib normalized.css

--- packages/aws-karuta/src/main.tsx
+++ packages/aws-karuta/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
-// import "normalize.css/normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
